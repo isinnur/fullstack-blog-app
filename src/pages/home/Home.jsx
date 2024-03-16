94e2d020f9b8d563ded277a44b5e5d19526b4edc
isinnur <isinngnyy@gmail.com>
2024-03-16 14:23:07 +0300

    feat: add navbar

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "../../header/Header";
+import "./home.css";
+export default function Home() {
+  return (
+    <div className="home">
+      <Header />
+      homepage
+    </div>
+  );
+}
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.css";
+
+export const TopBar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-square-facebook"></i>
+        <i className="topIcon fa-brands fa-square-x-twitter"></i>
+        <i className="topIcon fa-brands fa-square-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://images.pexels.com/photos/762080/pexels-photo-762080.jpeg?auto=compress&cs=tinysrgb&w=300"
+          alt=""
+        />
+        <i class="topSearc fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
+};
