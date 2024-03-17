0e3c2d78e68d11d7b8f0213e7f270a98f55d8a27
isinnur <isinngnyy@gmail.com>
2024-03-17 23:27:23 +0300

    feat: add express server

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+console.log("ışşınn backendcii olmak zooorr anaamm");
+app.listen("5000", () => {
+  console.log("Backend is running");
+});
