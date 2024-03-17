5c3ead82f51566545221fb8f06b4994073799c40
isinnur <isinngnyy@gmail.com>
2024-03-17 23:36:51 +0300

    add gitignore file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-console.log("ışşınn backendcii olmak zooorr anaamm");
 app.listen("5000", () => {
   console.log("Backend is running");
 });
