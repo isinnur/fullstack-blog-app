4336551b77d5af13c15cf3e093a830bfbcc06f5c
isinnur <isinngnyy@gmail.com>
2024-03-17 15:53:30 +0300

    feat: add write image

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,6 +3,11 @@ import "./write.css";
 export default function Write() {
   return (
     <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />{" "}
       <form className="writeForm">
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
