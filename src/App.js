8a8efedbd8cac197334a0b1aaa71dafabd633150
isinnur <isinngnyy@gmail.com>
2024-03-17 17:33:31 +0300

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Post from "./components/post/Post";
 
 function App() {
+  const user = true;
   return (
     <>
       <TopBar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      <Register />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/post/:postID" element={<Single />} />
+      </Routes>
     </>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import Header from "../../components/header/Header";
-import Posts from "../../posts/Posts";
+import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
 export default function Home() {
