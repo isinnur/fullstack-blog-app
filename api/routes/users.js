f36909d61fc9ec2b5d62b04d52bf51664b4f63f5
isinnur <isinngnyy@gmail.com>
2024-03-18 23:58:06 +0300

    feat(users): add delete function

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 //! UPDATE
 router.put("/:id", async (req, res) => {
@@ -26,4 +27,23 @@ router.put("/:id", async (req, res) => {
 });
 
 //! DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only your account!");
+  }
+});
+
 module.exports = router;
