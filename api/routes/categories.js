d10a3acba1b2ad0acd265d26e78104e692fbd0c7
isinnur <isinngnyy@gmail.com>
2024-03-19 19:39:12 +0300

    feat(categories): add get method

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -10,5 +10,13 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get("/", async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.status(200).json(cats);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
