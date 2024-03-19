c9238992bdbc07103f30f71ad5c17b3d2b0daa6c
isinnur <isinngnyy@gmail.com>
2024-03-19 19:27:12 +0300

    feat(categories): add category function

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+router.post("/", async (req, res) => {
+  const newCat = new Category(req.body);
+  try {
+    const savedCat = await newCat.save();
+    res.status(200).json(savedCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
