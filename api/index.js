c833c18df91e4372cb9319afd21c807f3fd06d39
isinnur <isinngnyy@gmail.com>
2024-03-19 19:27:42 +0300

    feat(index): add category route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 dotenv.config();
 app.use(express.json());
 
@@ -16,6 +17,7 @@ mongoose
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running");
