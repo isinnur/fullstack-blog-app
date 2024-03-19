6681f678c614ad7c48312d2e23969ae55d8ad160
isinnur <isinngnyy@gmail.com>
2024-03-19 19:13:43 +0300

    feat(posts): get all posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -69,4 +69,28 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//! GET ALL POST
+
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username: username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
