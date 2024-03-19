cd047b869c14b0de86c0eb9fc40eae6d2b4f8577
isinnur <isinngnyy@gmail.com>
2024-03-19 18:22:02 +0300

    feat(posts): delete post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -40,6 +40,24 @@ router.put("/:id", async (req, res) => {
 });
 
 //! DELETE POST
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //! GET POST
 // router.get("/:id", async (req, res) => {
 //   try {
