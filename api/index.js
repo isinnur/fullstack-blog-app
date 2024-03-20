06a8072a7b95ea42cb3d67e293dc72fc5def32a2
isinnur <isinngnyy@gmail.com>
2024-03-20 15:17:42 +0300

    feat(index): add multer

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 dotenv.config();
 app.use(express.json());
 
@@ -14,6 +15,23 @@ mongoose
   .then(console.log("connected to mongo"))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+app.post(
+  "/api/upload",
+  upload.single("file", (req, res) => {
+    res.status(200).json("File has been uploaded");
+  })
+);
+app.post;
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
