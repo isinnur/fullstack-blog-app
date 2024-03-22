512e649a61fbc8dc7ce0f94bad582b508df7fe22
isinnur <isinngnyy@gmail.com>
2024-03-22 14:52:44 +0300

    feat(register): add register system

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,31 +1,54 @@
 import { Link } from "react-router-dom";
 import "./register.css";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           className="registerInput"
           type="text"
           placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>Password</label>
         <input
           className="registerInput"
           type="password"
           placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button className="registerButton">
-          <Link className="link" to={"/register"}>
+          <Link className="link" to={"/register"} type="submit">
             Register
           </Link>
         </button>
@@ -35,6 +58,11 @@ export default function Register() {
           Login
         </Link>
       </button>
+      {error && (
+        <span style={{ color: "#D2691E", marginTop: "10px" }}>
+          Something went wrong
+        </span>
+      )}
     </div>
   );
 }
