1681272379c8fba753a0dd094a799a5dcd1025ff
isinnur <isinngnyy@gmail.com>
2024-03-26 14:32:05 +0300

    feat(Login): add login system

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,22 +1,47 @@
 import { Link } from "react-router-dom";
+import { useContext, useRef } from "react";
+import { Context } from "../../context/Contex";
+import axios from "axios";
 import "./login.css";
 
 export default function Login() {
+  //useRef
+  //dom elemanlarına doğrudan erişim sağlamak ve bileşenlerin ömrü boyunca değişmeyen değerleri saklamak
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+
+  console.log(isFetching);
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           className="loginInput"
           type="text"
           placeholder="Enter your username..."
+          ref={userRef}
         />
         <label>Password</label>
         <input
           className="loginInput"
-          type="text"
+          type="password"
           placeholder="Enter your password..."
+          ref={passwordRef}
         />
         <button className="loginButton" type="submit">
           Login
